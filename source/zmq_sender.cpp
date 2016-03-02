3e039c2d0cb8f19c7eb8a144ff88a96bf59f2c16
steppobeck <stephan.beck@uni-weimar.de>
2016-03-02 12:07:27 +0100

    added position from cmdline

--- source/zmq_sender.cpp
+++ source/zmq_sender.cpp
@@ -8,9 +8,15 @@
 
 int main(int argc, char* argv[]){
 
-
+  glm::vec3 position(0.0,0.0,0.0);
   CMDParser p("socket");
+  p.addOpt("p",3,"position", "specify the 3D position which should be send, default 0.0 0.0 0.0");
   p.init(argc,argv);
+
+  if(p.isOptSet("p")){
+    position = glm::vec3(p.getOptsFloat("p")[0], p.getOptsFloat("p")[1], p.getOptsFloat("p")[2]);
+    std::cout << "setting position to " << position << std::endl;
+  }
 
   std::string socket_name(p.getArgs()[0]);
 
@@ -27,16 +33,16 @@ int main(int argc, char* argv[]){
 
     glm::mat4 pose;
 
-    pose[3][0] = 0.0; // translate in x
-    pose[3][1] = 0.1 * tick; // translate in y
-    pose[3][2] = 0.0; // translate in z
+    pose[3][0] = position.x;
+    pose[3][1] = position.y;
+    pose[3][2] = position.z;
 
     zmq::message_t zmqm(sizeof(glm::mat4));
 
     memcpy( (unsigned char* ) zmqm.data(), (const unsigned char*) glm::value_ptr(pose), sizeof(glm::mat4));
     socket.send(zmqm);
 
-    std::cout << "sending: " << pose << std::endl;
+    //std::cout << "sending: " << pose << std::endl;
 
     ++tick;
   }
