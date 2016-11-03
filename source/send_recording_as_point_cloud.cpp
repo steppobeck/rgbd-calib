c5c31dee4953092f0222d0511f4a0f9f2ac541ba
steppobeck <stephan.beck@uni-weimar.de>
2016-11-03 16:58:09 +0100

    parallel reconstruction

--- source/send_recording_as_point_cloud.cpp
+++ source/send_recording_as_point_cloud.cpp
@@ -58,14 +58,82 @@ namespace{
     return false;
   }
 
+  float voxelsize_inv = 1.0f/0.008;
+  unsigned volume_width  = (bbx_max[0] - bbx_min[0])*voxelsize_inv;
+  unsigned volume_height = (bbx_max[1] - bbx_min[1])*voxelsize_inv;
+
+}
+
+void decomp_per_thread(const unsigned s_num, std::vector<unsigned char*>& tmp_rgba, std::vector<unsigned char*>& tmp_rgb, const RGBDConfig& cfg, RGBDSensor& sensor, const unsigned colorsize_tmp){
+  squish::DecompressImage (tmp_rgba[s_num], cfg.size_rgb.x, cfg.size_rgb.y,
+			   (unsigned char*) (s_num == 0 ? sensor.frame_rgb : sensor.slave_frames_rgb[s_num - 1]), squish::kDxt1);
+  // copy back rgbsensor
+  unsigned buffida = 0;
+  unsigned buffid = 0;
+  for(unsigned y = 0; y < cfg.size_rgb.y; ++y){
+    for(unsigned x = 0; x < cfg.size_rgb.x; ++x){
+      tmp_rgb[s_num][buffid++] = tmp_rgba[s_num][buffida++];
+      tmp_rgb[s_num][buffid++] = tmp_rgba[s_num][buffida++];
+      tmp_rgb[s_num][buffid++] = tmp_rgba[s_num][buffida++];
+      buffida++;
+    }
+  }
+  memcpy((unsigned char*) (s_num == 0 ? sensor.frame_rgb : sensor.slave_frames_rgb[s_num - 1]), tmp_rgb[s_num], colorsize_tmp);
+}
+
+void recon_per_thread(const unsigned tid, std::vector<CalibVolume*>& cvs, RGBDSensor& sensor, std::map<size_t, std::vector<voxel>>* recon_volume, const unsigned start_y, const unsigned end_y){
+  // do 3D reconstruction for each depth pixel
+
+  
+
+  for(unsigned y = start_y; y < end_y; ++y){
+    for(unsigned x = 0; x < (sensor.config.size_d.x - 3); ++x){
+
+      const unsigned d_idx = y* sensor.config.size_d.x + x;
+      float d = tid == 0 ? sensor.frame_d[d_idx] : sensor.slave_frames_d[tid - 1][d_idx];
+      if(d < cvs[tid]->min_d || d > cvs[tid]->max_d){
+	continue;
+      }
+
+      glm::vec3 pos3D;
+      glm::vec2 pos2D_rgb;
+      
+      pos3D = cvs[tid]->lookupPos3D( x * 1.0/sensor.config.size_d.x,
+				     y * 1.0/sensor.config.size_d.y, d);
+      
+      if(clip(pos3D)){
+	continue;
+      }
+
+
+      glm::vec2 pos2D_rgb_norm = cvs[tid]->lookupPos2D_normalized( x * 1.0/sensor.config.size_d.x, 
+								   y * 1.0/sensor.config.size_d.y, d);
+      pos2D_rgb = glm::vec2(pos2D_rgb_norm.x * sensor.config.size_rgb.x,
+			    pos2D_rgb_norm.y * sensor.config.size_rgb.y);
+	  
+      glm::vec3 rgb = sensor.get_rgb_bilinear_normalized(pos2D_rgb, tid);
+      
+      // discretize voxel here
+      voxel vxl;
+      vxl.xyz[0] = (unsigned short)((pos3D.x - bbx_min[0])*voxelsize_inv);
+      vxl.xyz[1] = (unsigned short)((pos3D.y - bbx_min[1])*voxelsize_inv);
+      vxl.xyz[2] = (unsigned short)((pos3D.z - bbx_min[2])*voxelsize_inv);
+      // compute grid_loc of surface point
+      const size_t grid_loc = vxl.xyz[0] + volume_width * vxl.xyz[1] + volume_width * volume_height * vxl.xyz[2];
+      vxl.rgbi[0] = (unsigned char) std::max(0.0f , std::min(255.0f, rgb.x * 255.0f));
+      vxl.rgbi[1] = (unsigned char) std::max(0.0f , std::min(255.0f, rgb.y * 255.0f));
+      vxl.rgbi[2] = (unsigned char) std::max(0.0f , std::min(255.0f, rgb.z * 255.0f));
+      (*recon_volume)[grid_loc].push_back(vxl);
+    }
+  }
 }
 
 
 // ./send_recording_as_point_cloud /mnt/pitoti/kinect_recordings/25.8b/23.cv /mnt/pitoti/kinect_recordings/25.8b/24.cv /mnt/pitoti/kinect_recordings/25.8b/25.cv /mnt/pitoti/kinect_recordings/25.8b/26.cv -s /mnt/pitoti/kinect_recordings/25.8b/session_1.stream -c -bbx -0.5 0.0 -0.5 0.5 2.0 0.5 -u 127.0.0.1 7002 -p1k 20 -p2k 3
 int main(int argc, char* argv[]){
 
-  float voxelsize = 0.008;
-  unsigned num_threads = 16;
+  
+  
   bool rgb_is_compressed = false;
   std::string stream_filename;
   unsigned wait_ms = 0;
@@ -73,7 +141,6 @@ int main(int argc, char* argv[]){
 
   CMDParser p("basefilename_cv .... ");
   p.addOpt("s",1,"stream_filename", "specify the stream filename which should be converted");
-  p.addOpt("n",1,"num_threads", "specify how many threads should be used, default 16");
   p.addOpt("c",-1,"rgb_is_compressed", "enable compressed support for rgb stream, default: false (not compressed)");
 
   p.addOpt("bbx",6,"bounding_box", "specify the bounding box x_min y_min z_min x_max y_max z_max in meters, default -1.2 -0.05 -1.2 1.2 2.4 1.2");
@@ -109,10 +176,6 @@ int main(int argc, char* argv[]){
     return 0;
   }
 
-  if(p.isOptSet("n")){
-    num_threads = p.getOptsInt("n")[0];
-  }
-
   if(p.isOptSet("c")){
     rgb_is_compressed = true;
   }
@@ -122,14 +185,12 @@ int main(int argc, char* argv[]){
   }
 
   if(p.isOptSet("v")){
-    voxelsize = p.getOptsFloat("v")[0];
+    voxelsize_inv = 1.0f/p.getOptsFloat("v")[0];
   }
 
   // compute volume dimensions
-  float voxelsize_inv = 1.0f/voxelsize;
-  const unsigned volume_width  = (bbx_max[0] - bbx_min[0])*voxelsize_inv;
-  const unsigned volume_height = (bbx_max[1] - bbx_min[1])*voxelsize_inv;
-  const unsigned volume_depth  = (bbx_max[2] - bbx_min[2])*voxelsize_inv;
+  volume_width  = (bbx_max[0] - bbx_min[0])*voxelsize_inv;
+  volume_height = (bbx_max[1] - bbx_min[1])*voxelsize_inv;
   
 
 
@@ -150,16 +211,21 @@ int main(int argc, char* argv[]){
   cfg.size_d   = glm::uvec2(512, 424);
   RGBDSensor sensor(cfg, num_streams - 1);	
 
-  unsigned char* tmp_rgb = 0;
-  unsigned char* tmp_rgba = 0;
+
+  const unsigned colorsize = rgb_is_compressed ? 691200 : cfg.size_rgb.x * cfg.size_rgb.y * 3;
+  const unsigned depthsize = cfg.size_d.x * cfg.size_d.y * sizeof(float);
+
+  std::vector<unsigned char*> tmp_rgb;
+  std::vector<unsigned char*> tmp_rgba;
   const unsigned colorsize_tmp = cfg.size_rgb.x * cfg.size_rgb.y * 3;
   const unsigned colorsize_tmpa = cfg.size_rgb.x * cfg.size_rgb.y * 4;
   if(rgb_is_compressed){
-    tmp_rgb = new unsigned char [colorsize_tmp];
-    tmp_rgba = new unsigned char [colorsize_tmpa];
-  }	
-  const unsigned colorsize = rgb_is_compressed ? 691200 : cfg.size_rgb.x * cfg.size_rgb.y * 3;
-  const unsigned depthsize = cfg.size_d.x * cfg.size_d.y * sizeof(float);
+    for(unsigned s_num = 0; s_num < num_streams; ++s_num){
+      tmp_rgb.push_back(new unsigned char [colorsize_tmp]);
+      tmp_rgba.push_back(new unsigned char [colorsize_tmpa]);
+    }
+  }
+
 
   FileBuffer fb(stream_filename.c_str());
   if(!fb.open("r")){
@@ -180,90 +246,61 @@ int main(int argc, char* argv[]){
     std::cout << "frame time: " << (ts_now - ts).msec() << " ms" << std::endl;
     ts = ts_now;
 
-
+    unsigned bytes_per_frame = 0;
     // start reading frames from file
     for(unsigned s_num = 0; s_num < num_streams; ++s_num){
       fb.read((unsigned char*) (s_num == 0 ? sensor.frame_rgb : sensor.slave_frames_rgb[s_num - 1]), colorsize);
-
+      bytes_per_frame += colorsize;
       if(s_num == 0){
 	memcpy((char*) &curr_frame_time, (const char*) sensor.frame_rgb, sizeof(double));
 	//std::cout << "curr_frame_time: " << curr_frame_time << std::endl;
       }
-
-      // start deconpress, if rgb is compressed
-      if(rgb_is_compressed){
-         // uncompress to rgb_tmp from (unsigned char*) (s_num == 0 ? sensor.frame_rgb : sensor.slave_frames_rgb[s_num - 1]) to tmp_rgba
-         squish::DecompressImage (tmp_rgba, cfg.size_rgb.x, cfg.size_rgb.y,
-                                  (unsigned char*) (s_num == 0 ? sensor.frame_rgb : sensor.slave_frames_rgb[s_num - 1]), squish::kDxt1);
-         // copy back rgbsensor
-         unsigned buffida = 0;
-         unsigned buffid = 0;
-         for(unsigned y = 0; y < cfg.size_rgb.y; ++y){
-	   for(unsigned x = 0; x < cfg.size_rgb.x; ++x){
-	     tmp_rgb[buffid++] = tmp_rgba[buffida++];
-	     tmp_rgb[buffid++] = tmp_rgba[buffida++];
-	     tmp_rgb[buffid++] = tmp_rgba[buffida++];
-	     buffida++;
-           }
-         }
-         memcpy((unsigned char*) (s_num == 0 ? sensor.frame_rgb : sensor.slave_frames_rgb[s_num - 1]), tmp_rgb, colorsize_tmp);
-      }
-
-
       fb.read((unsigned char*) (s_num == 0 ? sensor.frame_d : sensor.slave_frames_d[s_num - 1]), depthsize);
+      bytes_per_frame += depthsize;
     }
+
+    if(rgb_is_compressed){
+      boost::thread_group threadGroup;
+      for(unsigned s_num = 0; s_num < num_streams; ++s_num){
+	threadGroup.create_thread([s_num, &tmp_rgba, &tmp_rgb, &cfg, &sensor, colorsize_tmp](){
+				    decomp_per_thread(s_num, tmp_rgba, tmp_rgb, cfg, sensor, colorsize_tmp);
+				  }
+				  );
+      }
+      threadGroup.join_all();
+    }
+
 
     sensor::timevalue ts_now_read = sensor::clock::time();
     std::cout << "frame time_read: " << (ts_now_read - ts_now).msec() << " ms" << std::endl;
 
-    std::map<size_t, std::vector<voxel>> recon_volume;
-    for(unsigned s_num = 0; s_num < num_streams; ++s_num){
-      // do 3D reconstruction for each depth pixel
-      for(unsigned y = 0; y < sensor.config.size_d.y; ++y){
-	for(unsigned x = 0; x < (sensor.config.size_d.x - 3); ++x){
 
-	  const unsigned d_idx = y* sensor.config.size_d.x + x;
-	  float d = s_num == 0 ? sensor.frame_d[d_idx] : sensor.slave_frames_d[s_num - 1][d_idx];
-	  if(d < cvs[s_num]->min_d || d > cvs[s_num]->max_d){
-	    continue;
-	  }
+    const unsigned num_threads = num_streams * 2;
+    std::vector< std::map<size_t, std::vector<voxel>>> recon_volumes(num_threads);
+    boost::thread_group threadGroup;
+    unsigned tid = 0;
+    for(unsigned s_num = 0; s_num != num_streams; ++s_num){
+      threadGroup.create_thread([s_num, &cvs, &sensor, &recon_volumes, tid](){
+				  recon_per_thread(s_num, cvs, sensor, &(recon_volumes[tid]), 0, sensor.config.size_d.y/2);
+						   }
+				);
+      ++tid;
+      threadGroup.create_thread([s_num, &cvs, &sensor, &recon_volumes, tid](){
+				  recon_per_thread(s_num, cvs, sensor, &(recon_volumes[tid]), sensor.config.size_d.y/2, sensor.config.size_d.y);
+						   }
+				);
+      ++tid;
 
-	  glm::vec3 pos3D;
-	  glm::vec2 pos2D_rgb;
-	  
-	  pos3D = cvs[s_num]->lookupPos3D( x * 1.0/sensor.config.size_d.x,
-					   y * 1.0/sensor.config.size_d.y, d);
-
-	  if(clip(pos3D)){
-	    continue;
-	  }
-
-
-	  glm::vec2 pos2D_rgb_norm = cvs[s_num]->lookupPos2D_normalized( x * 1.0/sensor.config.size_d.x, 
-									 y * 1.0/sensor.config.size_d.y, d);
-	  pos2D_rgb = glm::vec2(pos2D_rgb_norm.x * sensor.config.size_rgb.x,
-				pos2D_rgb_norm.y * sensor.config.size_rgb.y);
-	  
-	  glm::vec3 rgb = sensor.get_rgb_bilinear_normalized(pos2D_rgb, s_num);
-
-	  // discretize voxel here
-	  voxel vxl;
-	  vxl.xyz[0] = (unsigned short)((pos3D.x - bbx_min[0])*voxelsize_inv);
-	  vxl.xyz[1] = (unsigned short)((pos3D.y - bbx_min[1])*voxelsize_inv);
-	  vxl.xyz[2] = (unsigned short)((pos3D.z - bbx_min[2])*voxelsize_inv);
-	  // compute grid_loc of surface point
-	  const size_t grid_loc = vxl.xyz[0] + volume_width * vxl.xyz[1] + volume_width * volume_height * vxl.xyz[2];
-	  vxl.rgbi[0] = (unsigned char) std::max(0.0f , std::min(255.0f, rgb.x * 255.0f));
-	  vxl.rgbi[1] = (unsigned char) std::max(0.0f , std::min(255.0f, rgb.y * 255.0f));
-	  vxl.rgbi[2] = (unsigned char) std::max(0.0f , std::min(255.0f, rgb.z * 255.0f));
-	  recon_volume[grid_loc].push_back(vxl);
-	}
-      }
     }
+    threadGroup.join_all();
+
+
     // filter recon_volume
     std::vector<voxel> voxels;
-    for(const auto& vxls : recon_volume){
-      voxels.push_back(vxls.second.front());
+    for(const auto& recon_volume : recon_volumes){
+      for(const auto& vxls : recon_volume){
+	voxels.push_back(vxls.second.front());
+      }
     }
     unsigned voxel_count = voxels.size();
     std::cout << "voxel_count: " << voxel_count << std::endl;
@@ -294,7 +331,7 @@ int main(int argc, char* argv[]){
     size_t buff_index = 0;
     unsigned packet_number = 0;
     unsigned voxel_number = 0;
-
+    unsigned bytes_sent_per_frame = 0;
     for(const auto& s : voxels){
 
       if(voxel_number == max_voxels_per_packet){
@@ -302,7 +339,7 @@ int main(int argc, char* argv[]){
 		  << " -> " << voxel_number
 		  << " (" << buff_index << " bytes)" << std::endl;
 	sender->send(buff, buff_index);
-	
+	bytes_sent_per_frame += buff_index;
 	if(wait_ms)
 	  sleep(sensor::timevalue::const_100_us * wait_ms);
 	
@@ -356,12 +393,15 @@ int main(int argc, char* argv[]){
 		<< " -> " << voxel_number
 		<< " (" << buff_index << " bytes)" << std::endl;
       sender->send(buff, buff_index);
+      bytes_sent_per_frame += buff_index;
     }
 
     sensor::timevalue ts_now_send = sensor::clock::time();
     std::cout << "frame time_send: " << (ts_now_send - ts_now_recon).msec() << " ms" << std::endl;
     // finished sending
     
+    // print some additional stats
+    std::cout << "kbytes_sent_per_frame (kbytes_per_frame): " << bytes_sent_per_frame/1024 << " (" << bytes_per_frame/1024 << ") -> " << bytes_sent_per_frame * 100.0f/bytes_per_frame << "%" << std::endl;
 
     if(frame_num == num_frames){
       frame_num = 0;
@@ -370,6 +410,15 @@ int main(int argc, char* argv[]){
 
 
   }
+
+
+  if(rgb_is_compressed){
+    for(unsigned s_num = 0; s_num < num_streams; ++s_num){
+      delete [] tmp_rgb[s_num];
+      delete [] tmp_rgba[s_num];
+    }
+  }
+
   
   return 0;
 }
