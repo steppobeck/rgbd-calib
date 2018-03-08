97d3a0829d10c4c055ec06200263a1957964662b
steppobeck <stephan.beck@uni-weimar.de>
2018-03-08 14:30:25 +0100

    added new functionality for 4K display

--- source/feedback_sender.cpp
+++ source/feedback_sender.cpp
@@ -12,6 +12,10 @@
 #include <iostream>
 #include <chrono>         // std::chrono::seconds
 #include <thread>
+
+
+
+
 struct feedback{
   glm::mat4 cyclops_mat;
   glm::mat4 screen_mat;
@@ -21,27 +25,69 @@ struct feedback{
 
 int main(int argc, char* argv[]){
 
-  unsigned head_tracking_id = 0;
+
   unsigned model_tracking_id = 0;
-  CMDParser p("socket");
-  p.addOpt("g",1,"glasses_tracking_id", "default: 0");
+  unsigned art_port = 5000;
+  unsigned start_port = 9000;
+  std::string sender_ip = "127.0.0.1";
+
+  CMDParser p("glass_ids");
   p.addOpt("m",1,"model_tracking_id", "default: 0");
   p.addOpt("r",1,"reconmodefilename", "default: none");
+  p.addOpt("a",1,"artport", "default: 5000");
+  p.addOpt("s",1,"senderip", "default: 127.0.0.1");
+  p.addOpt("p",1,"port", "default: 9000");
+
 
   p.init(argc,argv);
-
-  if(p.isOptSet("g")){
-    head_tracking_id = p.getOptsInt("g")[0];
-  }
 
   if(p.isOptSet("m")){
     model_tracking_id = p.getOptsInt("m")[0];
   }
 
+  if(p.isOptSet("a")){
+    art_port = p.getOptsInt("a")[0];
+  }
+
+  if(p.isOptSet("p")){
+    start_port = p.getOptsInt("p")[0];
+  }
+
+  if(p.isOptSet("s")){
+    sender_ip = p.getOptsString("s")[0];
+  }
+
+
+
   sensor::FileValue* recon_mode = 0;
   if(p.isOptSet("r")){
     recon_mode = new sensor::FileValue(p.getOptsString("r")[0].c_str());
   }
+
+
+
+  // prepare tracking system
+  sensor::device* d = sensor::devicemanager::the()->get_dtrack(art_port, sensor::timevalue::const_050_ms);
+  sensor::sensor* model = new sensor::sensor(d, model_tracking_id);
+
+  zmq::context_t ctx(1); // means single threaded
+  uint32_t hwm = 1;
+  std::vector<zmq::socket_t*> sockets;
+  std::vector<sensor::sensor*> glasses;
+  // parse glass_ids and generate socket endpoints
+  for(const auto& g_id : p.getArgs()){
+     std::string endpoint = std::string("tcp://" + sender_ip + ":" + std::to_string(start_port));
+     ++start_port;
+     zmq::socket_t* socket = new zmq::socket_t(ctx, ZMQ_PUB); // means a publisher
+     socket->setsockopt(ZMQ_SNDHWM,&hwm, sizeof(hwm));
+     socket->bind(endpoint.c_str());
+     sockets.push_back(socket);
+     glasses.push_back(new sensor::sensor(d, std::stoi(g_id)));
+     std::cout << "sending glass id " << std::stoi(g_id) << " to endpoint " << endpoint <<  std::endl;
+  }
+
+
+#if 0
   std::string socket_name(p.getArgs()[0]);
 
   zmq::context_t ctx(1); // means single threaded
@@ -53,16 +99,16 @@ int main(int argc, char* argv[]){
   socket.bind(endpoint.c_str());
 
   // prepare tracking system
-  sensor::device* d = sensor::devicemanager::the()->get_dtrack(5000, sensor::timevalue::const_050_ms);
+  sensor::device* d = sensor::devicemanager::the()->get_dtrack(art_port, sensor::timevalue::const_050_ms);
   sensor::sensor* head = new sensor::sensor(d, head_tracking_id);
   sensor::sensor* model = new sensor::sensor(d, model_tracking_id);
-
+#endif
   
 
   glm::mat4 scale_mat;
-  scale_mat[0][0] = 0.19;
-  scale_mat[1][1] = 0.19;
-  scale_mat[2][2] = 0.19;
+  scale_mat[0][0] = 0.25;
+  scale_mat[1][1] = 0.25;
+  scale_mat[2][2] = 0.25;
 
   glm::mat4 trans_mat;
   trans_mat[3][0] = 0.0;
@@ -75,9 +121,9 @@ int main(int argc, char* argv[]){
   fb.cyclops_mat[3][1] = 1.8;
   fb.cyclops_mat[3][2] = 0.0;
 
-  fb.screen_mat[3][0] = 0.0;
-  fb.screen_mat[3][1] = 1.5;
-  fb.screen_mat[3][2] = -1.7;
+  fb.screen_mat[3][0] = 0.0; // -0.05
+  fb.screen_mat[3][1] = 1.445; // 1.24
+  fb.screen_mat[3][2] = -2.03;
 
   // initial model is at center screen position
   fb.model_mat = fb.screen_mat * (trans_mat * scale_mat);
@@ -85,22 +131,36 @@ int main(int argc, char* argv[]){
 
   while(true){
 
+#if 0
     // receive data from tracking system
     if((head_tracking_id) != 0 && (model_tracking_id != 0)){
       fb.cyclops_mat = head->getMatrix();
       fb.model_mat = model->getMatrix() * (trans_mat * scale_mat);
     }
+#endif
 
     if(recon_mode != 0){
       recon_mode->read(fb.recon_mode);
     }
 
+    fb.model_mat = model->getMatrix() * (trans_mat * scale_mat);
+    for(unsigned i = 0; i != glasses.size(); ++i){
+       fb.cyclops_mat = glasses[i]->getMatrix();
+       zmq::message_t zmqm(sizeof(feedback));
+       memcpy( (unsigned char* ) zmqm.data(), (const unsigned char*) &fb, sizeof(feedback));
+       sockets[i]->send(zmqm);
+    }
+
+#if 0
     zmq::message_t zmqm(sizeof(feedback));
     memcpy( (unsigned char* ) zmqm.data(), (const unsigned char*) &fb, sizeof(feedback));
     socket.send(zmqm);
+#endif
+
     std::this_thread::sleep_for( std::chrono::milliseconds(16) );
 
   }
 
   return 0;
 }
+
